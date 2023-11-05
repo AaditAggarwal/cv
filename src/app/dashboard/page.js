463ba0ea3cd6a8efb8e52c81b6e97a33c875a9a6
Aadit Aggarwal <113587949+AaditAggarwal@users.noreply.github.com>
2023-11-05 12:18:27 -0600

    Create page.js

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+const dashboard = ({ searchParams }) => {
+    console.log(searchParams.search) // Logs "search"
+    
+    return (
+        <div>
+        </div>
+        
+    );
+}
+
+export default dashboard
